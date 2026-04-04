646fd6e941ca3bb95856da9a8384185599ced197
vocsong <vocsong>
2026-04-04 11:42:42 +0800

    fix: correct database name in stats reset migration comment

--- migrations/0006_stats_reset.sql
+++ migrations/0006_stats_reset.sql
@@ -1,5 +1,5 @@
 -- Run manually once after deploying 0005_add_elo.sql:
---   wrangler d1 execute sg-bridge-db --file=migrations/0006_stats_reset.sql
+--   wrangler d1 execute sg-bridge-users --file=migrations/0006_stats_reset.sql
 UPDATE users SET elo = 1000, wins = 0, games_played = 0;
 DELETE FROM elo_history;
 DELETE FROM game_records;
